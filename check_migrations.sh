6877a89b4d7a64e55f9fd7627d9667442e9f5a2b
McCleary <smccleary@archmi.com>
2024-04-12 12:47:43 -0500

    Look at migration logs and see if they all finished and how long it took

---
+++ check_migrations.sh
@@ -0,0 +1,23 @@
+echo -n "Checking migration logs for completion status..."
+
+# Now check to see how many were started
+MIGRATION_STARTED_COUNT=$(grep "Migration started" migration-log-*.log | wc -l)
+
+# And how many completed
+MIGRATION_COMPLETED_COUNT=$(grep "Migration complete" migration-log-*.log | wc -l)
+
+echo "done."
+echo "${MIGRATION_STARTED_COUNT}/${MIGRATION_COMPLETED_COUNT} migrations completed."
+
+if [ "${MIGRATION_STARTED_COUNT}" -ne "${MIGRATION_COMPLETED_COUNT}" ]; then
+  echo "Exiting because not all migrations completed."
+  exit 1
+fi
+
+echo -n "Checking duration..."
+
+MIGRATION_STARTED_TIME=$(sed -n "s/^\[\(.\+\)\] INFO -- Migration started.\+$/\\1/p" *.log | sort | head -1)
+MIGRATION_ENDED_TIME=$(sed -n "s/^\[\(.\+\)\] INFO -- Migration complete/\\1/p" *.log | sort | tail -n 1)
+
+echo "done."
+echo "Migration started at ${MIGRATION_STARTED_TIME} and ended at ${MIGRATION_ENDED_TIME}"
