a0f91d911c4eec2772de6ce8a5998c0f88f0132e
McCleary <smccleary@archmi.com>
2024-04-23 15:31:18 -0500

    Updated to work with GHES

--- migrate_secrets.sh
+++ migrate_secrets.sh
@@ -6,6 +6,7 @@
 # -t [destination system token]
 # -z [override destination org with this value; useful for testing]
 # -y [prepend prefix to destination repo names; useful for testing]
+# -a [GHES hostname (not API URL)]
 
 # TODO: Add parameter for source system API in case of migrating from GHES
 
@@ -15,6 +16,7 @@ set -e
 # Set defaults
 OVERRIDE_DESTINATION_ORG=""
 OVERRIDE_DESTINATION_REPO_PREFIX=""
+API_URL=""
 # Check options
 while getopts "i:o:s:t:a:p:w:z:y:" o; do
   case "${o}" in
@@ -27,6 +29,9 @@ while getopts "i:o:s:t:a:p:w:z:y:" o; do
 	t)
 	  DESTINATION_TOKEN=${OPTARG}
 	  ;;
+	a)
+	  API_URL=${OPTARG}
+	  ;;
 	z)
 	  OVERRIDE_DESTINATION_ORG=${OPTARG}
 	  ;;
@@ -36,7 +41,7 @@ while getopts "i:o:s:t:a:p:w:z:y:" o; do
   esac
 done
 
-if [ -z "${INPUT_FILE}" ] || [ -z "${SOURCE_TOKEN}" ] || [ -z "${DESTINATION_TOKEN}" ]; then
+if [ -z "${INPUT_FILE}" ] || [ -z "${SOURCE_TOKEN}" ] || [ -z "${DESTINATION_TOKEN}" ] || [ -z "${API_URL}" ]; then
   echo "Not all required parameters are provided.  View the top of this source file to see comment."
   exit 1;
 fi
@@ -54,14 +59,14 @@ do
 
   echo "Fetching secrets for ${SOURCE_ORG}/${SOURCE_REPO}"
   set +e
-  for SECRET_NAME in $(GITHUB_TOKEN="${SOURCE_TOKEN}" gh secret list --repo ${SOURCE_ORG}/${SOURCE_REPO} | tail -n +2 | cut -d$'\t' -f1)
+  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  # IMPORTANT:  Verify API results when fetching from GHEC vs GHES.  There seems to be a different header you need to skip with the "tail" command!
+  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  for SECRET_NAME in $(GH_HOST="${API_URL}" GH_ENTERPRISE_TOKEN="${SOURCE_TOKEN}" gh secret list --repo ${SOURCE_ORG}/${SOURCE_REPO} | tail -n +1 | cut -d$'\t' -f1)
   do
     echo "${SECRET_NAME} -> ${DESTINATION_ORG}/${DESTINATION_REPO}"
 	GITHUB_TOKEN="${DESTINATION_TOKEN}" gh secret set "${SECRET_NAME}" --body placeholder --repo ${DESTINATION_ORG}/${DESTINATION_REPO}
   done
   set -e
-  
-#  GITHUB_TOKEN="${SOURCE_TOKEN}" gh variable list --repo ${SOURCE_ORG}/${SOURCE_REPO}
-  
  
 done < "${INPUT_FILE}"
